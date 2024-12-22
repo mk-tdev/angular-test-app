3697681d14246e840cb2020f4219556fa4527cf5
Muthu Kumar <mk-dev@outlook.com>
2024-12-22 15:59:51 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserListComponent } from './features/user-list/user-list.component';
+import { TodoListComponent } from './features/todo-list/todo-list.component';
+import { PracticeComponent } from './features/practice/practice.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    PracticeComponent,
+    UserListComponent,
+    TodoListComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'test-ng-app';
+  constructor() {}
+
+  title = 'angular-app';
 }
